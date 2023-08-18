db7787bb1e7e819f5fd7c3ef6cebdc7a7d040599
muhammad-shayan <shayanksa@gmail.com>
2023-08-18 21:40:25 +0500

    added amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "inspirationalquotes": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "inspirationalquotes1ffe16d0": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "inspirationalQuotesLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+const moment = require("moment");
+const DateTime = moment().utc().format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
+console.log(DateTime);
